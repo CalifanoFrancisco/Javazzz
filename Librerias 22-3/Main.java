c37b4574e3849ff619aab2a5eb6da72b807f0ff2
Kaligrámetro <81710127+CalifanoFrancisco@users.noreply.github.com>
2022-03-23 00:03:32 -0300

    Update Main.java

--- Librerias 22-3/Main.java
+++ Librerias 22-3/Main.java
@@ -5,13 +5,13 @@ public class Main {
 
   public static void main(String[] args) {
 
-	Libro l1 = new Libro("Nahe",100,"ElAteneo"		);
-	Libro l2 = new Libro("pepe",200,"Kapelusz"		);
+	Libro l1 = new Libro("Nahe",100,"ElAteneo"	);
+	Libro l2 = new Libro("pepe",200,"Kapelusz"	);
 	Libro l3 = new Libro("sech",300,"Sudamericana"	);
 	Libro l4 = new Libro("hecg",400,"Atlántida"   	);
 	Libro l5 = new Libro("pole",500,"Interzona"   	);
-	Libro l6 = new Libro("caño",600,"Sur"			);
-	Libro l7 = new Libro("cali",700,"Alianza"		);
+	Libro l6 = new Libro("caño",600,"Sur"		);
+	Libro l7 = new Libro("cali",700,"Alianza"	);
 
 	Cliente c1 = new Cliente("mati",19,l1);
 
