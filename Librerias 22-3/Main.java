6062fc911b293564a97c0797696c70fca4d30f38
Kaligrámetro <81710127+CalifanoFrancisco@users.noreply.github.com>
2022-03-23 00:03:17 -0300

    Update Main.java

--- Librerias 22-3/Main.java
+++ Librerias 22-3/Main.java
@@ -16,6 +16,14 @@ public class Main {
 	Cliente c1 = new Cliente("mati",19,l1);
 
 	SistemaLibreria libreria = new SistemaLibreria("LibreriasNashe",l1,c1);
+	
+	libreria.addLibro(l2);
+	libreria.addLibro(l3);
+	libreria.addLibro(l4);
+	libreria.addLibro(l5);
+	libreria.addLibro(l6);
+	libreria.addLibro(l7); 
+	  
 	libreria.addClienteLibro(0, l2);
 	libreria.addClienteLibro(0, l3);
 	libreria.addClienteLibro(0, l3);
