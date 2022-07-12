9ab9d551c5f8f3a09fa84c992874474d41508253
Kaligrámetro <81710127+CalifanoFrancisco@users.noreply.github.com>
2022-07-12 15:55:32 -0300

    Add files via upload

---
+++ 12-7/Libreria.java
@@ -0,0 +1,8 @@
+import Libros.*;
+
+public class Libreria implements 
+Libros.Ficcion, Libros.Romance, Libros.Aventura, Libros.Ciencia, Libros.Distopia, Libros.Humanidades,
+Libros.Novela, Libros.Poesia, Libros.Policial, Libros.Tecnologia
+{
+
+}
